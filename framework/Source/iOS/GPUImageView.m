04f603bb332b43b937bfdc8b0dd0ee4fa32aef14
yangwenjie <364930126@qq.com>
2020-09-29 23:39:31 +0800

    【BestPartners】fix main thread warn

--- framework/Source/iOS/GPUImageView.m
+++ framework/Source/iOS/GPUImageView.m
@@ -230,6 +230,7 @@
 #pragma mark -
 #pragma mark Handling fill mode
 
+/* BestPartners: Modify By BestPartners，这里是原方法，已注释，新方法中将self.bounds.size提取到主线程访问
 - (void)recalculateViewGeometry;
 {
     runSynchronouslyOnVideoProcessingQueue(^{
@@ -278,6 +279,57 @@
 //        -1.0f,  1.0f,
 //        1.0f,  1.0f,
 //    };
+} */
+
+//BestPartners: Modify By BestPartners，这里是改动后方法，将self.bounds提取到主线程访问
+- (void)recalculateViewGeometry;
+{
+    __block CGRect currentBounds;
+    
+    dispatch_async(dispatch_get_main_queue(), ^(void){
+        //主要改动，self.bounds放到主线程中访问，后续使用currentBounds代替
+        currentBounds = self.bounds;
+        
+        runSynchronouslyOnVideoProcessingQueue(^{
+            CGFloat heightScaling, widthScaling;
+            
+            CGSize currentViewSize = currentBounds.size;
+            
+            //    CGFloat imageAspectRatio = inputImageSize.width / inputImageSize.height;
+            //    CGFloat viewAspectRatio = currentViewSize.width / currentViewSize.height;
+            
+            CGRect insetRect = AVMakeRectWithAspectRatioInsideRect(inputImageSize, currentBounds);
+            
+            switch(_fillMode)
+            {
+                case kGPUImageFillModeStretch:
+                {
+                    widthScaling = 1.0;
+                    heightScaling = 1.0;
+                }; break;
+                case kGPUImageFillModePreserveAspectRatio:
+                {
+                    widthScaling = insetRect.size.width / currentViewSize.width;
+                    heightScaling = insetRect.size.height / currentViewSize.height;
+                }; break;
+                case kGPUImageFillModePreserveAspectRatioAndFill:
+                {
+                    //            CGFloat widthHolder = insetRect.size.width / currentViewSize.width;
+                    widthScaling = currentViewSize.height / insetRect.size.height;
+                    heightScaling = currentViewSize.width / insetRect.size.width;
+                }; break;
+            }
+            
+            imageVertices[0] = -widthScaling;
+            imageVertices[1] = -heightScaling;
+            imageVertices[2] = widthScaling;
+            imageVertices[3] = -heightScaling;
+            imageVertices[4] = -widthScaling;
+            imageVertices[5] = heightScaling;
+            imageVertices[6] = widthScaling;
+            imageVertices[7] = heightScaling;
+        });
+    });
 }
 
 - (void)setBackgroundColorRed:(GLfloat)redComponent green:(GLfloat)greenComponent blue:(GLfloat)blueComponent alpha:(GLfloat)alphaComponent;
